db3ba19ec25d031c6bdac6c36d993825abb4be79
Sebastiaan Luca <hello@sebastiaanluca.com>
2021-03-11 17:04:27 +0100

    Remove the deleted views from the service provider
    This was giving some troubles when trying to publish the file or cache the views when deploying. `https://github.com/spatie/laravel-personal-data-export/blob/1.3.2/resources/views/mail.blade.php` was deleted in v2. The readme is also outdated.
    
    Can you tag a release when you get this merged?
    
    Thanks!

--- src/PersonalDataExportServiceProvider.php
+++ src/PersonalDataExportServiceProvider.php
@@ -14,13 +14,7 @@ class PersonalDataExportServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../config/personal-data-export.php' => config_path('personal-data-export.php'),
             ], 'config');
-
-            $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/personal-data-export'),
-            ], 'views');
         }
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'personal-data-export');
 
         Route::macro('personalDataExports', function (string $url) {
             Route::get("$url/{zipFilename}", '\Spatie\PersonalDataExport\Http\Controllers\PersonalDataExportController@export')
